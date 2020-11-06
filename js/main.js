49a1614243494db015139492ab35941641f6dc2b
IlariaGalliano <ilaria.galliano7@gmail.com>
2020-11-06 15:50:03 +0100

    vue-hello
    Completo

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
 /**
  *
  * Hello Vue
- * BONUS aggiungere un'immagine presa anch'essa da un data (url dell'immagine)
+ *
  *
  */
 
 // Stampare a schermo un messaggio all'interno di un h1, utilizzando i data.
+// BONUS aggiungere un'immagine presa anch'essa da un data (url dell'immagine)
+
   const app = new Vue({
     el: '#root',
     data: {
-      titolo: 'Hello vue'
+      titolo: 'Hello Vue',
+      imm: 'https://miro.medium.com/max/900/1*vYKA-nnA_hvBZil2TTq37g.jpeg'
     }
   });
