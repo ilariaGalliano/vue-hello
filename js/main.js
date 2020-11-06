cef6a786210895241f49daaa37daf5b87a3ce362
IlariaGalliano <ilaria.galliano7@gmail.com>
2020-11-06 15:23:31 +0100

    vue-hello
    primo punto

---
+++ js/main.js
@@ -0,0 +1,14 @@
+/**
+ *
+ * Hello Vue
+ * BONUS aggiungere un'immagine presa anch'essa da un data (url dell'immagine)
+ *
+ */
+
+// Stampare a schermo un messaggio all'interno di un h1, utilizzando i data.
+  const app = new Vue({
+    el: '#root',
+    data: {
+      titolo: 'Hello vue'
+    }
+  });
